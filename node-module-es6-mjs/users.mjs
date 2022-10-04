3290da8420c882a559a08dedc4fc32fbe0725e01
DimaSurdiy <dsurdiy@ukr.net>
2022-10-04 06:21:34 +0300

    add es6 modules

---
+++ node-module-es6-mjs/users.mjs
@@ -0,0 +1,9 @@
+const admins = ["Alex", "Andrey", "Vasyl"];
+const clients = ["Anna", "Alina", "Tamara"];
+
+const users = {
+  admins,
+  clients,
+};
+
+export default users;
---
+++ node-module-es6-type/date/isLeapYear.js
@@ -0,0 +1,3 @@
+const isLeapYear = (year) => {};
+
+export default isLeapYear;
