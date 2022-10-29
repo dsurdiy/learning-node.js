48a19f52799db45a43cdc1420bf2e9962c71a184
DimaSurdiy <dsurdiy@ukr.net>
2022-10-29 06:54:26 +0300

    update auth middleware

--- auth-example/middlewares/authenticate.js
+++ auth-example/middlewares/authenticate.js
@@ -10,14 +10,14 @@ const authenticate = async (req, res, next) => {
   try {
     const { authorization = "" } = req.headers;
     const [bearer, token] = authorization.split(" ");
-    if (bearer !== "Bearer") {
+    if (bearer !== "Bearer" || !token) {
       throw RequestError(401);
     }
 
     const { id } = jwt.verify(token, SECRET_KEY);
 
     const user = await User.findById(id);
-    if (!user) {
+    if (!user || user.token !== token) {
       throw RequestError(401);
     }
 
