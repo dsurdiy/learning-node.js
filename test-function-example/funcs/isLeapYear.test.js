a984a9d49fb880a76066cad8bf5a4437354801f1
DimaSurdiy <dsurdiy@ukr.net>
2022-11-04 07:12:19 +0200

    add starting test example files

---
+++ test-function-example/funcs/isLeapYear.test.js
@@ -0,0 +1,20 @@
+/*
+    1. Give integer number, return true if leap year and false if not.
+    2. if give wrong data throw error with specific message.
+
+    2008 - true
+    2003 - false
+    1900 - false
+    2000 - true
+    
+    41 - error 'year must be 42 or more'
+    2008.4 - error 'year must be integer'
+    () - 'error must be exist'
+    '2008' - error 'year must be number'
+    false - error 'year must be number'
+    true - error 'year must be number'
+    null - error 'year must be number'
+    ()=>{} - error 'year must be number'
+    {} - error 'year must be number'
+    [] - error 'year must be number'
+*/
