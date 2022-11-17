988aca134919dcd60d2e44d715665be1f3d0c518
DimaSurdiy <dsurdiy@ukr.net>
2022-11-17 08:33:38 +0200

    add router get all tasks

--- api/index.js
+++ api/index.js
@@ -5,4 +5,27 @@ require("dotenv").config();
 const router = express.Router();
 const uriDb = process.env.DB_HOST;
 
+router.get("/tasks", async (req, res, next) => {
+  const client = await new MongoClient(uriDb, {
+    useUnifiedTopology: true,
+  }).connect();
+
+  try {
+    const results = await client.db().collection("todos").find().toArray();
+
+    res.json({
+      status: "success",
+      code: 200,
+      data: {
+        tasks: results,
+      },
+    });
+  } catch (e) {
+    console.error(e);
+    next(e);
+  } finally {
+    await client.close();
+  }
+});
+
 module.exports = router;
