b98d47d0789ed0feb5e1a8d5627fad985a6faae8
DimaSurdiy <dsurdiy@ukr.net>
2022-11-19 18:34:10 +0200

    add router patch task status

--- api/index.js
+++ api/index.js
@@ -115,4 +115,37 @@ router.put("/tasks/:id", async (req, res, next) => {
   }
 });
 
+router.patch("/tasks/:id/status", async (req, res, next) => {
+  const { id } = req.params;
+  const { isDone = false } = req.body;
+
+  const client = await new MongoClient(uriDb, {
+    useUnifiedTopology: true,
+  }).connect();
+
+  try {
+    const objectId = new ObjectId(id);
+
+    const { value: result } = await client
+      .db()
+      .collection("todos")
+      .findOneAndUpdate(
+        { _id: objectId },
+        { $set: { isDone } },
+        { returnDocument: "after" }
+      );
+
+    res.json({
+      status: "success",
+      code: 200,
+      data: { task: result },
+    });
+  } catch (e) {
+    console.error(e);
+    next(e);
+  } finally {
+    await client.close();
+  }
+});
+
 module.exports = router;
